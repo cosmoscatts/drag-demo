7258622154b00cf3e87e59584caa97e47f815fa9
cosmoscatts <2138889191@qq.com>
2024-03-23 15:19:52 +0800

    chore: update

--- src/components/meta2d/props/Meta2dPenProps.vue
+++ src/components/meta2d/props/Meta2dPenProps.vue
@@ -1,32 +1,55 @@
 <script lang="ts" setup>
+import type { Pen } from '@meta2d/core'
+
 const { selections } = useMeta2dSelection()
 
-const pen = ref<any>()
-// 位置数据。当前版本位置需要动态计算获取
-const rect = ref<any>()
-
-const form = reactive({})
-
-onMounted(() => {
-  getPen()
+const form = reactive<any>({
+  id: '',
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  //
+  text: '',
+  color: '',
+  background: '',
+  dash: 0,
+  borderRadius: 0,
+  globalAlpha: 1,
+  textAlign: '',
+  textBaseline: '',
 })
 
 function getPen() {
-  pen.value = selections.pen
-  if (pen.value.globalAlpha === undefined) {
-    pen.value.globalAlpha = 1
+  const pen = selections.pen
+  if (!pen) return
+  form.id = pen.id || ''
+  form.text = pen.text || ''
+  form.color = pen.color || ''
+  form.background = pen.background || ''
+  form.dash = (pen as any).dash || 0
+  form.borderRadius = pen.borderRadius || 0
+  form.globalAlpha = pen.globalAlpha || 1
+  form.textAlign = pen.textAlign || 'center'
+  form.textBaseline = pen.textBaseline || 'middle'
+
+  const rect = meta2d.getPenRect(pen as Pen)
+  if (rect) {
+    form.x = rect.x || 0
+    form.y = rect.y || 0
+    form.width = rect.width || 0
+    form.height = rect.height || 0
   }
-  rect.value = meta2d?.getPenRect?.(pen.value)
 }
 
 // 监听选中不同图元
-const watcher = watch(() => selections.pen?.id, getPen)
+watch(() => selections.pen?.id, getPen)
 
 const lineDashs = [undefined, [5, 5]]
 
 function changeValue(prop: string) {
-  const v: any = { id: pen.value.id }
-  v[prop] = pen.value[prop]
+  const v: any = { id: form.id }
+  v[prop] = form[prop]
   if (prop === 'dash') {
     v.lineDash = lineDashs[v[prop]]
   }
@@ -34,57 +57,38 @@ function changeValue(prop: string) {
 }
 
 function changeRect(prop: string) {
-  const v: any = { id: pen.value.id }
-  v[prop] = rect.value[prop]
+  const v: any = { id: form.id }
+  v[prop] = form[prop]
   meta2d.setValue(v, { render: true })
 }
 
-// function top() {
-//   meta2d.top()
-//   meta2d.render()
-// }
-// function bottom() {
-//   meta2d.bottom()
-//   meta2d.render()
-// }
-// function up() {
-//   meta2d.up()
-//   meta2d.render()
-// }
-// function down() {
-//   meta2d.down()
-//   meta2d.render()
-// }
-
-onUnmounted(() => {
-  watcher()
-})
+onMounted(getPen)
 </script>
 
 <template>
   <div>
-    <a-form v-if="pen" :model="form" auto-label-width label-align="left" size="small">
+    <a-form v-if="selections.pen" :model="form" auto-label-width label-align="left" size="small">
       <h5 mb-4 font-bold>
         图元
       </h5>
       <a-form-item label="文本" name="text">
-        <a-input v-model="pen.text" @change="changeValue('text')" />
+        <a-input v-model="form.text" @change="changeValue('text')" />
       </a-form-item>
       <!-- <a-form-item label="颜色" name="color">
-        <a-color-picker v-model="pen.color" w-full @change="changeValue('color')" />
+        <a-color-picker v-model="form.color" w-full @change="changeValue('color')" />
       </a-form-item> -->
       <!-- <a-form-item label="背景" name="background">
-        <a-color-picker v-model="pen.background" w-full @change="changeValue('background')" />
+        <a-color-picker v-model="form.background" w-full @change="changeValue('background')" />
       </a-form-item> -->
       <a-form-item label="线条" name="dash">
-        <a-select v-model="pen.dash" @change="changeValue('dash')">
+        <a-select v-model="form.dash" @change="changeValue('dash')">
           <a-option :key="0" :value="0" label="实线" />
           <a-option :key="1" :value="1" label="虚线" />
         </a-select>
       </a-form-item>
       <a-form-item label="圆角" name="borderRadius">
         <a-input-number
-          v-model="pen.borderRadius"
+          v-model="form.borderRadius"
           :min="0"
           :max="1"
           hide-button
@@ -93,44 +97,41 @@ onUnmounted(() => {
       </a-form-item>
       <a-form-item label="不透明度" name="globalAlpha">
         <a-slider
-          v-model="pen.globalAlpha"
+          v-model="form.globalAlpha"
           :min="0"
           :max="1"
           :step="0.01"
           @change="changeValue('globalAlpha')"
         />
-        <span class="ml-16" style="width: 50px; line-height: 30px">
-          {{ pen.globalAlpha }}
-        </span>
       </a-form-item>
 
       <a-divider />
 
       <a-form-item label="X" name="x">
-        <a-input-number v-model="rect.x" hide-button @change="changeRect('x')" />
+        <a-input-number v-model="form.x" hide-button @change="changeRect('x')" />
       </a-form-item>
       <a-form-item label="Y" name="y">
-        <a-input-number v-model="rect.y" hide-button @change="changeRect('y')" />
+        <a-input-number v-model="form.y" hide-button @change="changeRect('y')" />
       </a-form-item>
       <a-form-item label="宽" name="width">
-        <a-input-number v-model="rect.width" hide-button @change="changeRect('width')" />
+        <a-input-number v-model="form.width" hide-button @change="changeRect('width')" />
       </a-form-item>
       <a-form-item label="高" name="height">
-        <a-input-number v-model="rect.height" hide-button @change="changeRect('height')" />
+        <a-input-number v-model="form.height" hide-button @change="changeRect('height')" />
       </a-form-item>
 
       <a-divider />
 
-      <a-form-item label="文字水平对齐" name="textAlign">
-        <a-select v-model="pen.textAlign" @change="changeValue('textAlign')">
+      <a-form-item label="文字水平" name="textAlign">
+        <a-select v-model="form.textAlign" @change="changeValue('textAlign')">
           <a-option key="left" value="left" label="左对齐" />
           <a-option key="center" value="center" label="居中" />
           <a-option key="right" value="right" label="右对齐" />
         </a-select>
       </a-form-item>
-      <a-form-item label="文字垂直对齐" name="textBaseline">
+      <a-form-item label="文字垂直" name="textBaseline">
         <a-select
-          v-model="pen.textBaseline"
+          v-model="form.textBaseline"
           @change="changeValue('textBaseline')"
         >
           <a-option key="top" value="top" label="顶部对齐" />
@@ -138,23 +139,6 @@ onUnmounted(() => {
           <a-option key="bottom" value="bottom" label="底部对齐" />
         </a-select>
       </a-form-item>
-
-      <a-divider />
-
-      <!-- <a-space>
-        <a-button @click="top">
-          置顶
-        </a-button>
-        <a-button @click="bottom">
-          置底
-        </a-button>
-        <a-button @click="up">
-          上一层
-        </a-button>
-        <a-button @click="down">
-          下一层
-        </a-button>
-      </a-space> -->
     </a-form>
   </div>
 </template>
--- src/pages/meta2d/index.vue
+++ src/pages/meta2d/index.vue
@@ -33,12 +33,12 @@ provide('preview', preview)
 <template>
   <div>
     <Meta2dToolbar ref="refToolbar" v-model="showProps" v-model:visible="previewVisible" />
-    <div grid="~ cols-7" border="1 base" :style="{ height: contentHeight }">
+    <div grid="~ cols-6" border="1 base" :style="{ height: contentHeight }">
       <div col-span-1 border="r-1 base" :style="{ height: contentHeight }" of-y-auto>
         <Meta2dGraphics />
       </div>
       <Meta2dContextMenu>
-        <Meta2dView v-if="!previewVisible" :style="{ height: contentHeight, width: '100%' }" :class="showProps ? 'col-span-5' : 'col-span-6'" />
+        <Meta2dView v-if="!previewVisible" :style="{ height: contentHeight, width: '100%' }" :class="showProps ? 'col-span-4' : 'col-span-5'" />
       </Meta2dContextMenu>
       <div v-show="showProps" col-span-1 border="l-1 base" :style="{ height: contentHeight }" of-y-auto>
         <Meta2dProps />

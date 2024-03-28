8a2f2deee69f903c37bb7db81376b018cff346e7
cosmoscatts <2138889191@qq.com>
2024-03-28 16:45:19 +0800

    chore: update

--- src/modules/arco.ts
+++ src/modules/arco.ts
@@ -1,5 +1,4 @@
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import ArcoVue from '@arco-design/web-vue'
-import '@arco-design/web-vue/dist/arco.css'
 
 export const install: UserModule = ({ use }) => use(ArcoVueIcon).use(ArcoVue)
--- src/scripts/setup-assets.ts
+++ src/scripts/setup-assets.ts
@@ -3,5 +3,6 @@ import '@unocss/reset/tailwind.css'
 import '~/styles/main.less'
 import 'uno.css'
 import 'vue-draggable-resizable/style.css'
+import '@arco-design/web-vue/dist/arco.css'
 
 export default function setupAssets() {}
